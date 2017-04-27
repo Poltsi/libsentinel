e078331b44c01332736345d3be17fe5594074977
Paul-Erik Törrönen <poltsi@poltsi.fi>
2017-04-27 07:26:21 +0300

    * Explicitly note that connect_sentinel and open_sentinel take void arguments
    Signed-off-by: Paul-Erik Törrönen <poltsi@poltsi.fi>

--- include/libsentinel.h
+++ include/libsentinel.h
@@ -9,8 +9,8 @@
 #ifndef libsentinel_h__
 #define libsentinel_h__
 
-extern int connect_sentinel();
-extern int open_sentinel_device();
+extern int connect_sentinel(void);
+extern int open_sentinel_device(void);
 extern bool send_sentinel_command(int fd, char *command);
 extern bool read_sentinel_data(int fd, char *buffer);
 extern bool disconnect_sentinel(int fd);

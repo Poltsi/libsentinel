4a78255498cf58d56196da5ed18e301ccd69ff65
Paul-Erik Törrönen <poltsi@poltsi.fi>
2017-04-30 10:08:18 +0300

    * Add structs for the dive data * Add dprint macro * Rename get_sentinel_header to download_sentinel_header * Add parse_sentinel_header and get_sentinel_dive_list * Create str_cut as internal function * In parse_sentinel_header, add 3 first field parsers, more to come

--- include/libsentinel.h
+++ include/libsentinel.h
@@ -3,6 +3,7 @@
 #include <stdarg.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <termios.h>
 #include <unistd.h>
@@ -15,11 +16,86 @@ static const char SENTINEL_LIST_CMD[]  = {0x4d}; // d command to list the dive h
 static const char SENTINEL_WAIT_BYTE[] = {0x50}; // P the rebreather prints this when it is waiting for a command
 
 /* Macros */
-#define dprint(verbose, ...) ( verbose ? printf(__VA_ARGS__) : 0) 
+#define dprint(verbose, ...) ( verbose ? printf(__VA_ARGS__) : 0)
+
+/* Structs */
+typedef struct sentinel_gas {
+    int n2; /* Nitrogen percentage */
+    int he; /* Helium percentage */
+    int o2; /* O2 percentage, we calculate this from the previous two */
+    long max_depth; /* Configured max depth for gas, originally given in decimeter */
+    int enabled; /* Whether the gas is enabledx */
+} sentinel_gas_t;
+
+typedef struct sentinel_tissue {
+    int t1; /* Unknown */
+    int t2; /* Unknown */
+} sentinel_tissue_t;
+
+typedef struct sentinel_note {
+    char *note; /* Original note */
+    int type; /* Type of note, 0 == info, 1 == warning, 2 == error */
+} sentinel_note_t;
+
+typedef struct sentinel_dive_log_line {
+    int time_idx; /* Time index */
+    int time_s; /* Seconds since start of dive, equal to time_idx * record_interval */
+    char *time_string; /* Beautified time string in the format hh:mm:ss */
+    long depth; /* Converted to meter from decimeter */
+    long po2; /* Converted from hectobar */
+    int temperature;
+    long scrubber_left; /* Converted from promille to percentage. This is broken in the newer version */
+    long primary_battery_V; /* Converted from hectovolt, value for primary handset */
+    long secondary_battery_V; /* Converted from hectovolt, value for secondary handset */
+    int diluent_pressure; /* Diluent cylinder pressure */
+    int o2_pressure; /* Oxygen cylinder pressure */
+    long cell_o2[3]; /* pO2-reading for each cell */
+    long setpoint; /* Converted from hectobar to bar */
+    int ceiling; /* Decompression ceiling, m*/
+    sentinel_note_t note[3]; /* Info, warning and alerts, can be max 3 per log line */
+    long tempstick_value[8]; /* Converted from decicelsius, there are 8 sensors along the tempstick */
+    long co2; /* Converted from millibar to bar */
+} sentinel_dive_log_line_t;
+
+typedef struct sentinel_dive_header {
+    char *version;
+    int record_interval;
+    char *serial_number;
+    int log_lines; /* Last number of Mem */
+    int start_s; /* Original value converted to unixtime */
+    int end_s; /* Original value converted to unixtime */
+    char *start_time; /* Derived string representation from the start_s */
+    char *end_time; /* Derived string representation from the end_s */
+    float max_depth;
+    int otu;
+    int atm; /* Atmospheric pressure in mbar */
+    int stack; /* No information what this is, could have something to do with the scrubber */
+    int usage; /* Unknown */
+    double cns;
+    double safety; /* Unknown */
+    int expert; /* Unknown */
+    int tpm; /* Is the TPM enabled */
+    char *decoalg; /* Decompression algorithm */
+    double vgm_max_safety; /* Unknown */
+    double vgm_stop_safety; /* Unknown */
+    double vgm_mid_safety; /* Unknown */
+    int filter_type;  /* Unknown */
+    int cell_health[3]; /* Cell health for cell 1, 2 and 3*/
+    sentinel_gas_t gas[10]; /* Configured gasses */
+    sentinel_tissue_t tissue[16]; /* Not yet clear what these are */
+    sentinel_dive_log_line_t **log; /* Allocate this based on the log_lines */
+} sentinel_header_t;
+
+/* External functions */
 extern int connect_sentinel(char *devicex);
 extern int open_sentinel_device(char *device);
 extern bool send_sentinel_command(int fd, const char *command);
 extern bool read_sentinel_data(int fd, char *buffer);
 extern bool disconnect_sentinel(int fd);
-extern bool get_sentinel_header(int fd, char *buffer);
+extern bool download_sentinel_header(int fd, char *buffer);
+extern bool parse_sentinel_header(sentinel_header_t *header_struct, char *buffer);
+extern bool get_sentinel_dive_list(int fd, char *buffer);
+
+/* Internal functions */
+char **str_cut(char *orig_string, const char *delim);
 #endif  // LIBSENTINEL_H
--- src/libsentinel.c
+++ src/libsentinel.c
@@ -87,8 +87,142 @@ bool disconnect_sentinel(int fd) {
         return(false);
 }
 
-bool get_sentinel_header(int fd, char *buffer) {
+bool download_sentinel_header(int fd, char *buffer) {
     send_sentinel_command(fd, SENTINEL_LIST_CMD);
-    read_sentinel_data(fd, buffer);
-    return(false);
+    if (!read_sentinel_data(fd, buffer)) {
+        printf("ERROR: Failed to read data from Sentinel\n");
+        return(false);
+    }
+
+    return(true);
+}
+
+/* split_sentinel_header: Takes the raw header buffer and splits it to a string array, one item per dive */
+bool split_sentinel_header(char *buffer, char **head_array) {
+    int num_dive = 0;
+    char wbuf[sizeof(buffer)];
+    memcpy(wbuf, buffer, sizeof(&buffer));
+    // one which is at the end of it
+    char *dive_start = wbuf;
+    // Get the end of the first dive metadata
+    char *dive_end   = NULL;
+ 
+    do {
+        printf( "Checking dive: %d", num_dive );
+        dive_end = strstr( dive_start, "d\r\n" );
+        printf( "Reallocing head_array" );
+        head_array = realloc( head_array, ( num_dive + 1 ) * sizeof( *head_array ) );
+
+        int strsize = dive_end - dive_start;
+
+        if( dive_end == NULL ) {/* if there is only one, or if this is the last dive */
+            printf( "Last dive entry" );
+            strsize = ( wbuf + strlen( wbuf ) ) - dive_start;
+        }
+
+        strsize++;
+        printf( "New string length: %d", strsize );
+        printf( "Mallocing head_array entry" );
+        head_array[ num_dive ] = malloc( strsize * sizeof( char ) );
+        printf( "Resetting head_array entry" );
+        memset( head_array[ num_dive ], 0, strsize );
+        printf( "Copying head_array entry" );
+        strncpy( head_array[ num_dive ], dive_start, ( strsize - 1 ) );
+        dive_start = dive_start + strsize + 2; /* Move the start pointer to the beginning of next */
+        printf( "Header length: %d", strsize );
+        num_dive++;
+    }
+    while( dive_end != NULL );
+
+    return(true);
+}
+
+/**
+ * parse_sentinel_header: Parse a single dive header to the dive header
+ *                        struct from the given string buffer
+ */
+
+bool parse_sentinel_header(sentinel_header_t *header_struct, char *buffer) {
+    int buffer_size = strlen(buffer);
+    dprint(true, "Parsable buffer size: %d\n", buffer_size);
+    char **h_lines = str_cut(buffer, "\r\n"); /* Cut it by lines */
+    int line_idx = 0;
+
+    while(strlen(h_lines[line_idx])) {
+        if (strncmp(h_lines[line_idx], "ver=", 4)) {
+            header_struct->version = malloc((strlen(h_lines[line_idx]) - 4) * sizeof(char));
+            strncpy(header_struct->version, (h_lines[line_idx] + 4), (strlen(h_lines[line_idx]) - 4));
+            line_idx++;
+            continue;
+        }
+        if (strncmp(h_lines[line_idx], "Recint=", 7)) {
+            header_struct->record_interval = atoi(h_lines[line_idx] + 7);
+            line_idx++;
+            continue;
+        }
+        if (strncmp(h_lines[line_idx], "SN=", 3)) {
+            header_struct->serial_number = malloc((strlen(h_lines[line_idx]) - 3) * sizeof(char));
+            strncpy(header_struct->serial_number, (h_lines[line_idx] + 3), (strlen(h_lines[line_idx]) - 3));
+            line_idx++;
+            continue;
+        }
+    }
+
+    return(true);
+}
+
+bool get_sentinel_dive_list(int fd, char *buffer) {
+    if (!download_sentinel_header(fd, buffer)) {
+        printf("ERROR: Failed to get the Sentinel header\n");
+        return(false);
+    }
+
+    sentinel_header_t *header_list = malloc(sizeof(sentinel_header_t));
+
+    if (!parse_sentinel_header(header_list, buffer)) {
+        printf("ERROR: Failed parse the Sentinel header\n");
+        return(false);
+    }
+
+    return(true);
+}
+
+/*************************************************************************/
+/* Minor helper functions used internally                                */
+/*************************************************************************/
+
+char **str_cut(char *orig_string, const char *delim) {
+    char **str_array  = NULL; /* We store the splits here */
+    int arr_idx = 0; /* Our index counter for str_array */
+    char *start_ptr   = orig_string;
+    char *end_ptr     = orig_string;
+    const int win_len = strlen(delim); /* This is our moving window length */
+    const int orig_len = strlen(orig_string);
+    /* We move the end_ptr one char at a time forward, and at each step we compare
+     * whether the next win_len chars are equal to the delim. If this is the case,
+     * then we know that the string between start and end ptr is to be stored in our
+     * str_array after which we move the start_ptr and end_ptr win_len forward. If
+     * we come across a delim while start and end ptr are equal, we skip since this
+     * is an empty string. The last item in the array is a 0 so we know that this is
+     * where it ends */
+
+    while ((end_ptr - orig_string) <= orig_len) {
+        if (strncmp((end_ptr + 1), delim, win_len)) {
+            if (start_ptr < end_ptr) {
+                str_array = realloc(str_array, (arr_idx + 1) * sizeof(*str_array));
+                str_array[arr_idx] = malloc((end_ptr - start_ptr + 1) * sizeof(char));
+                strncpy(str_array[arr_idx], start_ptr, (end_ptr - start_ptr));
+                arr_idx++;
+            } /* Else we skip adding to the str_array as the string length is 0*/
+
+            end_ptr += win_len; /* Jump over the delimiter string */
+            start_ptr = end_ptr;
+        } else {
+            end_ptr++;
+        }
+    }
+
+    str_array[arr_idx] = malloc(sizeof('\0'));
+    str_array[arr_idx] = '\0';
+    return(str_array);
 }
